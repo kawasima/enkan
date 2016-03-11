7ed9b6db1c45c890c4e8caf4dad9414d2167ba44
kawasima <kawasima1016@gmail.com>
2016-03-11 23:49:45 +0900

    Added a middleware for preventing the attack of click jacking

---
+++ enkan-web/src/main/java/enkan/middleware/FrameOptionsMiddleware.java
@@ -0,0 +1,54 @@
+package enkan.middleware;
+
+import enkan.MiddlewareChain;
+import enkan.annotation.Middleware;
+import enkan.collection.Parameters;
+import enkan.data.HttpRequest;
+import enkan.data.HttpResponse;
+import enkan.util.HttpResponseUtils;
+
+import java.util.Locale;
+
+/**
+ * Adds the X-Frame-Options header to the response.
+ *
+ * @author kawasima
+ */
+@Middleware(name = "frameOptions")
+public class FrameOptionsMiddleware extends AbstractWebMiddleware {
+    private Object frameOptions;
+
+
+    public FrameOptionsMiddleware() {
+        frameOptions = "sameorigin";
+    }
+
+    private String formatFrameOptions() {
+        if (frameOptions instanceof Parameters) {
+            return "ALLOW-FROM " + ((Parameters) frameOptions).get("allow-from");
+        } else {
+            return frameOptions.toString().toUpperCase(Locale.US);
+        }
+    }
+
+    @Override
+    public HttpResponse handle(HttpRequest request, MiddlewareChain chain) {
+        String headerValue = formatFrameOptions();
+        HttpResponse response = castToHttpResponse(chain.next(request));
+        HttpResponseUtils.header(response, "X-Frame-Options", headerValue);
+        return response;
+    }
+
+    public void setFrameOptions(String frameOptions) {
+        this.frameOptions = frameOptions;
+    }
+
+    public void setFrameOptions(Parameters frameOptions) {
+        if (!frameOptions.containsKey("allow-from")) {
+            // TODO Use MisconfigurationException
+            throw new IllegalArgumentException("frameOptions is allowed `ALLOW-FROM`.");
+        }
+        this.frameOptions = frameOptions;
+    }
+
+}
