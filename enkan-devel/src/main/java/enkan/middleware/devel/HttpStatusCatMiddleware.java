c4f156d9689763b41924817dcc24a46f4561cd49
kawasima <kawasima1016@gmail.com>
2016-05-17 17:02:06 +0900

    More cats!

--- enkan-devel/src/main/java/enkan/middleware/devel/HttpStatusCatMiddleware.java
+++ enkan-devel/src/main/java/enkan/middleware/devel/HttpStatusCatMiddleware.java
@@ -12,6 +12,7 @@ import net.unit8.moshas.context.Context;
 
 import java.io.StringWriter;
 import java.util.List;
+import java.util.Objects;
 
 import static enkan.util.HttpResponseUtils.getHeader;
 import static enkan.util.HttpResponseUtils.isEmptyBody;
@@ -22,16 +23,23 @@ import static net.unit8.moshas.RenderUtils.text;
  */
 @Middleware(name = "httpStatusCat", dependencies = {"contentType"})
 public class HttpStatusCatMiddleware extends AbstractWebMiddleware {
+    private boolean moreCats;
+
     private MoshasEngine moshas = new MoshasEngine();
+
     private Template template = moshas.describe("templates/httpStatusCat.html", t -> {
         t.select("head > title", text("status"));
         t.select("img#cat", (el, ctx) -> el.attr("src", "//http.cat/" + ctx.getString("status")));
     });
 
+    public HttpStatusCatMiddleware() {
+        moreCats = true;
+    }
+
     @Override
     public HttpResponse handle(HttpRequest request, MiddlewareChain next) {
         HttpResponse response = castToHttpResponse(next.next(request));
-        if (response != null && isEmptyBody(response)) {
+        if (response != null && (isEmptyBody(response) || isMoreCats(response))) {
             String type = getHeader(response, "Content-Type");
             if (type == null || "text/html".equals(type)) {
                 HttpResponseUtils.header(response, "Content-Type", "text/html");
@@ -44,5 +52,14 @@ public class HttpStatusCatMiddleware extends AbstractWebMiddleware {
             }
         }
         return response;
+    }
+
+    private boolean isMoreCats(HttpResponse response) {
+        return moreCats && response.getBody() instanceof String
+                && Objects.toString(response.getBody(), "").length() < 256;
+    }
+
+    public void setMoreCats(boolean moreCats) {
+        this.moreCats = moreCats;
     }
 }
