53e9a646b8282da087c60d677f786b6eaf4d6353
wada.mizuki <wada.mizuki@tis.co.jp>
2016-11-04 14:06:34 +0900

    Added Preflight Request setting. If HTTP Request is not CORS Request, it don't add headers.

---
+++ enkan-web/src/main/java/enkan/middleware/SimpleCORSMiddleware.java
@@ -0,0 +1,40 @@
+package enkan.middleware;
+
+import enkan.MiddlewareChain;
+import enkan.annotation.Middleware;
+import enkan.collection.Headers;
+import enkan.data.HttpRequest;
+import enkan.data.HttpResponse;
+
+import java.util.Objects;
+
+import static enkan.util.BeanBuilder.builder;
+
+/**
+ * CORS setting.
+ *
+ * @author syobochim
+ */
+@Middleware(name = "cors")
+public class SimpleCORSMiddleware extends AbstractWebMiddleware {
+    @Override
+    public HttpResponse handle(HttpRequest httpRequest, MiddlewareChain chain) {
+        if (Objects.isNull(httpRequest.getHeaders().get("Origin"))) {
+            return castToHttpResponse(chain.next(httpRequest));
+        }
+
+        if (Objects.equals(httpRequest.getRequestMethod(), "OPTIONS")
+                && httpRequest.getHeaders().containsKey("Access-Control-Request-Method")) {
+            HttpResponse<String> httpResponse = builder(HttpResponse.of("")).set(HttpResponse::setStatus, 200).build();
+            httpResponse.setHeaders(Headers.of("Access-Control-Allow-Origin", httpRequest.getHeaders().get("Origin"),
+                    "Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS",
+                    "Access-Control-Allow-Headers", "Content-Type",
+                    "Access-Control-Allow-Credentials", "true"));
+            return httpResponse;
+        }
+
+        HttpResponse httpResponse = castToHttpResponse(chain.next(httpRequest));
+        httpResponse.setHeaders(Headers.of("Access-Control-Allow-Origin", httpRequest.getHeaders().get("Origin")));
+        return httpResponse;
+    }
+}
---
+++ enkan-web/src/test/java/enkan/middleware/SimpleCORSMiddlewareTest.java
@@ -0,0 +1,91 @@
+package enkan.middleware;
+
+import enkan.Endpoint;
+import enkan.MiddlewareChain;
+import enkan.chain.DefaultMiddlewareChain;
+import enkan.collection.Headers;
+import enkan.data.DefaultHttpRequest;
+import enkan.data.HttpRequest;
+import enkan.data.HttpResponse;
+import enkan.predicate.AnyPredicate;
+import org.junit.Test;
+
+import static enkan.util.BeanBuilder.builder;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+/**
+ * {@link SimpleCORSMiddleware} Test.
+ *
+ * @author syobochim
+ */
+public class SimpleCORSMiddlewareTest {
+
+    @Test
+    public void testPreflightRequest() throws Exception {
+        // SetUp
+        HttpRequest request = builder(new DefaultHttpRequest()).build();
+        request.setRequestMethod("OPTIONS");
+        request.setHeaders(Headers.of("Origin", "http://sample.com",
+                "Access-Control-Request-Method", "POST"));
+
+        MiddlewareChain<HttpRequest, HttpResponse> chain = new DefaultMiddlewareChain<>(
+                new AnyPredicate<>(), null, (Endpoint<HttpRequest, HttpResponse>) req ->
+                builder(HttpResponse.of("")).set(HttpResponse::setStatus, 404).build());
+
+        // Exercise
+        SimpleCORSMiddleware sut = new SimpleCORSMiddleware();
+        HttpResponse result = sut.handle(request, chain);
+
+        // Verify
+        assertThat(result.getStatus(), is(200));
+        Headers headers = result.getHeaders();
+        assertThat(headers.get("Access-Control-Allow-Origin"), is("http://sample.com"));
+        assertThat(headers.get("Access-Control-Allow-Methods"), is("GET, POST, DELETE, PUT, OPTIONS"));
+        assertThat(headers.get("Access-Control-Allow-Headers"), is("Content-Type"));
+        assertThat(headers.get("Access-Control-Allow-Credentials"), is("true"));
+    }
+
+    @Test
+    public void testAddHeaders() throws Exception {
+        // SetUp
+        HttpRequest request = builder(new DefaultHttpRequest()).build();
+        request.setHeaders(Headers.of("Origin", "http://sample.com"));
+
+        MiddlewareChain<HttpRequest, HttpResponse> chain = new DefaultMiddlewareChain<>(
+                new AnyPredicate<>(), null, (Endpoint<HttpRequest, HttpResponse>) req ->
+                builder(HttpResponse.of("hello")).build());
+
+        // Exercise
+        SimpleCORSMiddleware sut = new SimpleCORSMiddleware();
+        HttpResponse result = sut.handle(request, chain);
+
+        // Verify
+        Headers headers = result.getHeaders();
+        assertThat(headers.get("Access-Control-Allow-Origin"), is("http://sample.com"));
+    }
+
+    @Test
+    public void testNonCORSRequest() throws Exception {
+        // Setup
+        HttpRequest request = builder(new DefaultHttpRequest()).build();
+        request.setHeaders(Headers.of("User-Agent", "Chrome"));
+
+        MiddlewareChain<HttpRequest, HttpResponse> chain = new DefaultMiddlewareChain<>(
+                new AnyPredicate<>(), null, (Endpoint<HttpRequest, HttpResponse>) req ->
+                builder(HttpResponse.of("hello")).build());
+
+        // Exercise
+        SimpleCORSMiddleware sut = new SimpleCORSMiddleware();
+        HttpResponse result = sut.handle(request, chain);
+
+        // Verify
+        Headers headers = result.getHeaders();
+        assertThat(headers.get("Access-Control-Allow-Origin"), nullValue());
+        assertThat(headers.get("Access-Control-Allow-Methods"), nullValue());
+        assertThat(headers.get("Access-Control-Allow-Headers"), nullValue());
+        assertThat(headers.get("Access-Control-Allow-Credentials"), nullValue());
+        assertThat(result.getBody(), is("hello"));
+    }
+
+}
