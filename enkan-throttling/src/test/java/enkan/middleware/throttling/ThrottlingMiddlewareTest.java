dace4c9b87bb16f3e614a0ca21c1dc429a2e4f2c
kawasima <kawasima1016@gmail.com>
2017-09-04 18:26:46 +0900

    Add tests of throttling

---
+++ enkan-throttling/src/test/java/enkan/middleware/throttling/ThrottlingMiddlewareTest.java
@@ -0,0 +1,96 @@
+package enkan.middleware.throttling;
+
+import enkan.Endpoint;
+import enkan.MiddlewareChain;
+import enkan.chain.DefaultMiddlewareChain;
+import enkan.data.DefaultHttpRequest;
+import enkan.data.HttpRequest;
+import enkan.data.HttpResponse;
+import enkan.predicate.AnyPredicate;
+import enkan.throttling.LimitRate;
+import enkan.throttling.Throttle;
+import org.apache.commons.lang3.RandomUtils;
+import org.junit.Test;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static enkan.util.BeanBuilder.builder;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertThat;
+
+public class ThrottlingMiddlewareTest {
+    @Test
+    public void sameIP() throws InterruptedException {
+        List<Throttle> throttles = Arrays.asList(
+                new Throttle("IP", new LimitRate(1, Duration.ofMillis(500L)),req -> req.getRemoteAddr())
+        );
+
+        ThrottlingMiddleware middleware = builder(new ThrottlingMiddleware())
+                .set(ThrottlingMiddleware::setThrottles, throttles)
+                .build();
+
+        DefaultHttpRequest req = new DefaultHttpRequest();
+        req.setRemoteAddr("127.0.0.1");
+        MiddlewareChain<HttpRequest, HttpResponse> chain = new DefaultMiddlewareChain(new AnyPredicate(), null,
+                (Endpoint<HttpRequest, HttpResponse>) request -> {
+                    return HttpResponse.of("");
+                });
+        final AtomicInteger count200 = new AtomicInteger(0);
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(3);
+        service.scheduleAtFixedRate(() -> {
+            HttpResponse res = middleware.handle(req, chain);
+            if (res.getStatus() == 200) {
+                count200.addAndGet(1);
+            }
+        }, 0, 250, TimeUnit.MILLISECONDS);
+
+        service.schedule(() -> service.shutdown(), 3, TimeUnit.SECONDS);
+        service.awaitTermination(5, TimeUnit.SECONDS);
+
+        assertThat(count200.get(), greaterThan(3));
+    }
+
+    @Test
+    public void randomIP() throws InterruptedException {
+        List<Throttle> throttles = Arrays.asList(
+                new Throttle("IP", new LimitRate(1, Duration.ofMillis(500L)),req -> req.getRemoteAddr())
+        );
+
+        ThrottlingMiddleware middleware = builder(new ThrottlingMiddleware())
+                .set(ThrottlingMiddleware::setThrottles, throttles)
+                .build();
+
+        DefaultHttpRequest req = new DefaultHttpRequest();
+        MiddlewareChain<HttpRequest, HttpResponse> chain = new DefaultMiddlewareChain(new AnyPredicate(), null,
+                (Endpoint<HttpRequest, HttpResponse>) request -> {
+                    return HttpResponse.of("");
+                });
+
+        final AtomicInteger count429 = new AtomicInteger(0);
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(3);
+        service.scheduleAtFixedRate(() -> {
+            req.setRemoteAddr(RandomUtils.nextInt(1, 255)
+                    + "." + RandomUtils.nextInt(1, 255)
+                    + "." + RandomUtils.nextInt(1, 255)
+                    + "." + RandomUtils.nextInt(1, 255)
+            );
+            HttpResponse res = middleware.handle(req, chain);
+            if (res.getStatus() == 429) {
+                count429.addAndGet(1);
+            }
+        }, 0, 250, TimeUnit.MILLISECONDS);
+
+        service.schedule(() -> service.shutdown(), 3, TimeUnit.SECONDS);
+        service.awaitTermination(5, TimeUnit.SECONDS);
+
+        assertThat(count429.get(), equalTo(0));
+    }
+
+}
