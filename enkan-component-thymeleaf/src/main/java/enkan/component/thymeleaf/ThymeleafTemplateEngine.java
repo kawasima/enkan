eed0b5c21393b773285e4a7ed243985cc02a0dd3
kawasima <kawasima1016@gmail.com>
2018-01-18 17:09:03 +0900

    Add setter for configuration of thymeleaf

--- enkan-component-thymeleaf/src/main/java/enkan/component/thymeleaf/ThymeleafTemplateEngine.java
+++ enkan-component-thymeleaf/src/main/java/enkan/component/thymeleaf/ThymeleafTemplateEngine.java
@@ -7,14 +7,22 @@ import enkan.util.HttpResponseUtils;
 import kotowari.component.TemplateEngine;
 import kotowari.data.TemplatedHttpResponse;
 import kotowari.io.LazyRenderInputStream;
+import org.thymeleaf.DialectConfiguration;
+import org.thymeleaf.cache.ICacheManager;
 import org.thymeleaf.context.Context;
+import org.thymeleaf.dialect.IDialect;
+import org.thymeleaf.linkbuilder.ILinkBuilder;
+import org.thymeleaf.messageresolver.IMessageResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import java.io.ByteArrayInputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 import java.util.function.Function;
 
 /**
@@ -26,13 +34,17 @@ public class ThymeleafTemplateEngine extends TemplateEngine {
     private ClassLoader classLoader;
     private String encoding = "UTF-8";
 
-    org.thymeleaf.TemplateEngine thymeleafEngine;
+    private Set<IDialect> dialects;
+    private Set<ITemplateResolver> templateResolvers;
+    private Set<IMessageResolver> messageResolvers;
+    private Set<ILinkBuilder> linkBuilders;
+
+    private ICacheManager cacheManager;
+
+    private org.thymeleaf.TemplateEngine thymeleafEngine;
 
     @Override
     public HttpResponse render(String name, Object... keyOrVals) {
-
-
-
         TemplatedHttpResponse response = TemplatedHttpResponse.create(name, keyOrVals);
         response.setBody(new LazyRenderInputStream(() -> {
             // FIXME set locale.
@@ -59,17 +71,34 @@ public class ThymeleafTemplateEngine extends TemplateEngine {
         return new ComponentLifecycle<ThymeleafTemplateEngine>() {
             @Override
             public void start(ThymeleafTemplateEngine component) {
-                if (classLoader == null) {
-                    classLoader = Thread.currentThread().getContextClassLoader();
-                }
-                ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver(classLoader);
-                resolver.setTemplateMode(TemplateMode.HTML);
-                resolver.setPrefix(prefix);
-                resolver.setSuffix(suffix);
-                resolver.setCharacterEncoding(encoding);
-
                 component.thymeleafEngine = new org.thymeleaf.TemplateEngine();
-                component.thymeleafEngine.setTemplateResolver(resolver);
+
+                // Template Resolver
+                if (component.templateResolvers == null) {
+                    component.thymeleafEngine.setTemplateResolver(component.createDefaultTemplateResolver());
+                } else {
+                    component.thymeleafEngine.setTemplateResolvers(component.templateResolvers);
+                }
+
+                // Dialect
+                if (component.dialects != null) {
+                    component.thymeleafEngine.setDialects(component.dialects);
+                }
+
+                // Message Resolver
+                if (component.messageResolvers != null) {
+                    component.thymeleafEngine.setMessageResolvers(component.messageResolvers);
+                }
+
+                // Link Builders
+                if (component.linkBuilders != null) {
+                    component.thymeleafEngine.setLinkBuilders(component.linkBuilders);
+                }
+
+                // Cache Manager
+                if (component.cacheManager != null) {
+                    component.thymeleafEngine.setCacheManager(component.cacheManager);
+                }
             }
 
             @Override
@@ -78,5 +107,49 @@ public class ThymeleafTemplateEngine extends TemplateEngine {
                 component.thymeleafEngine = null;
             }
         };
+    }
+
+    private ITemplateResolver createDefaultTemplateResolver() {
+        if (classLoader == null) {
+            classLoader = Thread.currentThread().getContextClassLoader();
+        }
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver(classLoader);
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setPrefix(prefix);
+        resolver.setSuffix(suffix);
+        resolver.setCharacterEncoding(encoding);
+        return resolver;
+    }
+
+    public void setDialects(Set<IDialect> dialects) {
+        this.dialects = dialects;
+    }
+
+    public void setTemplateResolvers(Set<ITemplateResolver> templateResolvers) {
+        this.templateResolvers = templateResolvers;
+    }
+
+    public void setMessageResolvers(Set<IMessageResolver> messageResolvers) {
+        this.messageResolvers = messageResolvers;
+    }
+
+    public void setLinkBuilders(Set<ILinkBuilder> linkBuilders) {
+        this.linkBuilders = linkBuilders;
+    }
+
+    public void setCacheManager(ICacheManager cacheManager) {
+        this.cacheManager = cacheManager;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+
+    public void setEncoding(String encoding) {
+        this.encoding = encoding;
     }
 }
