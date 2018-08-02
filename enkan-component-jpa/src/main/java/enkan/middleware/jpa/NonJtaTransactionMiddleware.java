844ca71781034403b38594b98d4edab605e729b6
kawasima <kawasima1016@gmail.com>
2018-08-02 23:35:11 +0900

    Enable to handle a request whose has no controller method

--- enkan-component-jpa/src/main/java/enkan/middleware/jpa/NonJtaTransactionMiddleware.java
+++ enkan-component-jpa/src/main/java/enkan/middleware/jpa/NonJtaTransactionMiddleware.java
@@ -11,8 +11,13 @@ import javax.transaction.Transactional;
 import java.lang.reflect.Method;
 import java.util.Optional;
 
-@Middleware(name = "nonJtaTransaction", dependencies = "entityManager")
+@Middleware(name = "nonJtaTransaction", dependencies = {"entityManager", "routing"})
 public class NonJtaTransactionMiddleware<REQ, RES> implements enkan.Middleware<REQ, RES, REQ, RES> {
+    private Transactional.TxType getTransactionType(Class<?> cls) {
+        Transactional transactional = cls.getDeclaredAnnotation(Transactional.class);
+        return transactional != null ? transactional.value() : null;
+    }
+
     private Transactional.TxType getTransactionType(Method m) {
         Transactional transactional = m.getDeclaredAnnotation(Transactional.class);
         return transactional != null ? transactional.value() : null;
@@ -27,22 +32,25 @@ public class NonJtaTransactionMiddleware<REQ, RES> implements enkan.Middleware<R
                 .orElseThrow(() -> new MisconfigurationException("eclipselink.NOT_ENTITY_MANAGEABLE_REQUEST"));
         if (req instanceof Routable) {
             Routable routable = (Routable) req;
-                Method m = routable.getControllerMethod();
-                Transactional.TxType type = getTransactionType(m);
-                if (type != null) {
-                    em.getTransaction().begin();
-                    try {
-                        RES ret = chain.next(req);
-                        em.getTransaction().commit();
-                        return ret;
-                    } catch (Throwable t) {
-                        if (em.getTransaction().isActive()) {
-                            em.getTransaction().rollback();
-                        }
-                        throw t;
+            Transactional.TxType type = getTransactionType(routable.getControllerClass());
+            Method m = routable.getControllerMethod();
+            if (m != null) {
+                type = Optional.ofNullable(getTransactionType(m)).orElse(type);
+            }
+            if (type != null) {
+                em.getTransaction().begin();
+                try {
+                    RES ret = chain.next(req);
+                    em.getTransaction().commit();
+                    return ret;
+                } catch (Throwable t) {
+                    if (em.getTransaction().isActive()) {
+                        em.getTransaction().rollback();
                     }
+                    throw t;
                 }
             }
-            return chain.next(req);
+        }
+        return chain.next(req);
     }
 }
