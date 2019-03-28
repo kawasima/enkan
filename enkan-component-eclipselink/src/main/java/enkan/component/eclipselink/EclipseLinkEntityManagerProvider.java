6219133b9665532395dcfab9c20c37b14d894815
kawasima <kawasima1016@gmail.com>
2019-03-28 13:48:27 +0900

    Bridge log to SLF4j

--- enkan-component-eclipselink/src/main/java/enkan/component/eclipselink/EclipseLinkEntityManagerProvider.java
+++ enkan-component-eclipselink/src/main/java/enkan/component/eclipselink/EclipseLinkEntityManagerProvider.java
@@ -5,11 +5,13 @@ import enkan.component.DataSourceComponent;
 import enkan.component.jpa.EntityManagerProvider;
 import org.eclipse.persistence.config.PersistenceUnitProperties;
 import org.eclipse.persistence.internal.jpa.deployment.SEPersistenceUnitInfo;
+import org.eclipse.persistence.logging.slf4j.SLF4JLogger;
 
 import javax.persistence.Persistence;
 import javax.persistence.spi.PersistenceUnitTransactionType;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -41,7 +43,6 @@ public class EclipseLinkEntityManagerProvider extends EntityManagerProvider<Ecli
                 pu.setPersistenceUnitRootUrl(getClass().getResource("/"));
                 pu.setTransactionType(PersistenceUnitTransactionType.RESOURCE_LOCAL);
                 pu.setNonJtaDataSource(getDataSource());
-
                 List<URL> jarFiles = managedClasses.stream()
                         .map(cls -> cls.getResource("/"))
                         .distinct()
@@ -53,9 +54,13 @@ public class EclipseLinkEntityManagerProvider extends EntityManagerProvider<Ecli
                         .collect(Collectors.toList());
                 pu.setManagedClassNames(managedClassNames);
                 pu.setExcludeUnlistedClasses(false);
+
                 getJpaProperties().put(PersistenceUnitProperties.ECLIPSELINK_SE_PUINFO, pu);
                 getJpaProperties().put(PersistenceUnitProperties.SESSION_NAME, UUID.randomUUID().toString());
                 getJpaProperties().put(PersistenceUnitProperties.LOGGING_LEVEL + ".sql", sqlLogLevel);
+                // Bridge to SLF4j
+                getJpaProperties().put(PersistenceUnitProperties.LOGGING_LOGGER, SLF4JLogger.class.getName());
+
                 component.setEntityManagerFactory(Persistence
                         .createEntityManagerFactory(getName(), getJpaProperties()));
             }
@@ -67,10 +72,29 @@ public class EclipseLinkEntityManagerProvider extends EntityManagerProvider<Ecli
         };
     }
 
+    /**
+     * Register a class managed by Eclipselink.
+     *
+     * @param managedClass A class managed by Eclipselink
+     */
     public void registerClass(Class<?> managedClass) {
         managedClasses.add(managedClass);
     }
 
+    /**
+     * Register classes managed by Eclipselink.
+     *
+     * @param classes classes managed by Eclipselink
+     */
+    public void registerClasses(Class<?>... classes) {
+        managedClasses.addAll(Arrays.asList(classes));
+    }
+
+    /**
+     * Set the logging level for executed SQLs.
+     *
+     * @param sqllogLevel the logging level
+     */
     public void setSqlLogLevel(String sqllogLevel) {
         this.sqlLogLevel = sqlLogLevel;
     }
